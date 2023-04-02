53956c76f6afec135d183fa45600ad96c768142f
Michele Santoro <michelesantoro2000@icloud.com>
2023-04-02 18:30:09 +0200

    Milestone 1 conclusa

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,43 @@
+<script>
+    import { store } from '../store.js';
+    export default {
+        name: "AppHeader",
+        data () {
+            return {
+                store
+            }
+        }
+    }
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <div class="title">BoolFlix</div>
+            <div class="filterWrapper">
+                <input type="text" placeholder="Cerca un film" v-model="store.filter" >
+                <button @click="$emit('searching')">Search</button>
+            </div>
+        </div>
+    </header>
+</template>
+
+
+<style scoped lang="scss">
+header {
+    background-color: black;
+    height: 80px;
+}
+.container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 100%;
+    margin: 0px 20px;
+}
+.title {
+
+    color: red;
+    font-size: 30px;
+}
+</style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,133 @@
+<script>
+    import { store } from '../store.js'
+    export default {
+        name: "MainPage",
+        data () {
+            return {
+                store
+            }
+        }
+    }
+</script>
+
+<template>
+<div class="container">
+    <div class="cardBox" v-for="(element, index) in this.store.FilmList">
+        <div class="card" >
+            <span class="text">
+                <img src="" alt="">
+            </span>
+            <div class="content">
+                <h1>{{ element.title }}</h1>
+                <h2>{{ element.original_title }}</h2>
+                <h4>{{ element.original_language }}</h4>
+                <h4>{{ element.vote_average }}</h4>
+            </div>
+        </div>
+    </div>
+</div>
+
+</template>
+
+<style scoped lang="scss">
+
+.container {
+    display: flex;
+    flex-wrap: wrap;
+}
+
+h1 {
+    font-size: 15px;
+}
+.cardBox {
+  width: 270px;
+  height: 300px;
+  position: relative;
+  display: grid;
+  place-items: center;
+  overflow: hidden;
+  border-radius: 20px;
+  box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 10px 0px,
+    rgba(0, 0, 0, 0.5) 0px 2px 25px 0px;
+}
+
+.card {
+  position: absolute;
+  width: 95%;
+  height: 95%;
+  background: #000814;
+  border-radius: 20px;
+  z-index: 5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+  color: #ffffff;
+  overflow: hidden;
+  padding: 20px;
+  cursor: pointer;
+  box-shadow: rgba(0, 0, 0, 0.4) 0px 30px 60px -12px inset,
+    rgba(0, 0, 0, 0.5) 0px 18px 36px -18px inset;
+}
+
+.card h3, span {
+  position: absolute;
+  top: 30%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 3rem;
+  font-weight: 800;
+  pointer-events: none;
+  opacity: 0.1;
+}
+
+.text {
+  top: 50%;
+}
+
+.card .content h3 {
+  font-size: 3rem;
+  padding-bottom: 10px;
+}
+
+.card .content p {
+  font-size: 1.6rem;
+  line-height: 25px;
+}
+
+.card .content {
+  transform: translateY(100%);
+  opacity: 0;
+  transition: 0.3s ease-in-out;
+}
+
+.card:hover .content {
+  transform: translateY(0);
+  opacity: 1;
+}
+
+.cardBox::before {
+  content: "";
+  position: absolute;
+  width: 40%;
+  height: 150%;
+  background: #40E0D0;
+  background: -webkit-linear-gradient(to right, #088c13, #FF8C00, #40E0D0);
+  background: linear-gradient(to right, #04a329, #FF8C00, #40E0D0);
+  animation: glowing01 5s linear infinite;
+  transform-origin: center;
+  animation: glowing 5s linear infinite;
+}
+
+@keyframes glowing {
+  0% {
+    transform: rotate(0);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+</style>
